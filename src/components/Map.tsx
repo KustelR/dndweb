545c64e08ee6ea01393993aa2a8813ab2756ccf2
KustelR <dmitriysstudios2@gmail.com>
2025-02-04 01:31:00 +0400

    added ability to move around map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,45 +2,134 @@
 
 import React, { useState, useEffect, useRef, Ref } from "react";
 import MapItem from "./MapItem";
+import { Rect, Vec2 } from "@/geometry";
+import { renderItem, findPointed, getRelativeClickPos } from "@/scripts/map";
 
 type MapItem = {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
+  pos: Rect;
   id: string;
 };
 
-type MapProps = {
-  data: Array<MapItem>;
-};
+export default function Map(props: MapProps) {
+  const { data, defaultOffset } = props;
 
-function relativePosition(
-  event: React.MouseEvent,
-  parentEl: HTMLDivElement,
-): { x: number; y: number } {
-  const bounds = parentEl.getBoundingClientRect();
+  const mapRoot = useRef<HTMLDivElement | null>(null);
+  const [items, setItems] = useState(data);
+  const [offset, setOffset] = useState(
+    defaultOffset ? defaultOffset : new Vec2(0, 0),
+  );
+  const [button, setButton] = useState(-1);
+  const [lastMove, setLastMove] = useState(new Vec2(0, 0));
+  const [bounds, setBounds] = useState(new Vec2(0, 0));
 
-  return {
-    x: event.clientX - bounds.left,
-    y: event.clientY - bounds.top,
-  };
+  useEffect(() => {
+    if (mapRoot.current) {
+      const clRect = mapRoot.current.getBoundingClientRect();
+      setBounds(new Vec2(clRect.x, clRect.y));
+    }
+  }, [mapRoot]);
+  useEffect(() => {
+    switch (button) {
+      case 1:
+        setOffset(offset.add(lastMove));
+        break;
+      default:
+        break;
+    }
+  }, [lastMove]);
+  useEffect(() => {}, [button]);
+
+  const moving: Array<string> = [];
+  const [movingElements, setMovingElements] = useState(moving);
+  return (
+    <div className="w-full h-full">
+      <div
+        className="relative w-full h-full overflow-hidden"
+        onMouseDown={(e) => {
+          if (mapRoot.current) {
+            setButton(e.button);
+            onMouseDown(e, bounds, offset, items, setMovingElements);
+          }
+        }}
+        onMouseUp={(e) => {
+          setButton(-1);
+          onMouseUp(e, setMovingElements);
+        }}
+        onMouseMove={(e) => {
+          setLastMove(new Vec2(e.movementX, e.movementY));
+          onMouseMove(e, movingElements, items, setItems);
+        }}
+      >
+        {items.map((item) => {
+          const { isVisible, pos } = renderItem(item.pos, offset, bounds);
+          return isVisible ? (
+            <div key={item.id} ref={mapRoot}>
+              <MapItem pos={pos}></MapItem>
+            </div>
+          ) : (
+            ""
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
+function getAbsolutePosition(pos: Vec2, offset: Vec2): Vec2 {
+  return pos.add(offset);
+}
+
+type MapProps = {
+  data: Array<MapItem>;
+  defaultOffset?: Vec2;
+};
 function onMouseDown(
   event: React.MouseEvent,
-  parentEl: HTMLDivElement,
+  bounds: Vec2,
+  offset: Vec2,
   data: Array<MapItem>,
   setMovingElements: (ids: Array<string>) => void,
 ): void {
-  const pointed = findPointed(relativePosition(event, parentEl), data);
+  const pressed = event.button;
 
+  switch (pressed) {
+    case 0: // left mouse button
+      handleElementDrag(event, bounds, offset, data, setMovingElements);
+      break;
+    default:
+      break;
+  }
+}
+
+function handleElementDrag(
+  event: React.MouseEvent,
+  bounds: Vec2,
+  offset: Vec2,
+  data: Array<MapItem>,
+  setMovingElements: (ids: Array<string>) => void,
+) {
+  const pointed = findPointed(
+    getRelativeClickPos(event, bounds),
+    data.map(
+      (item) =>
+        new Rect(
+          ...getAbsolutePosition(
+            new Vec2(item.pos.x, item.pos.y),
+            offset,
+          ).toArray(),
+          item.pos.width,
+          item.pos.height,
+        ),
+    ),
+  );
   if (pointed.length < 0) return;
 
   setMovingElements(
-    pointed.map((el) => {
-      return el.id;
-    }),
+    pointed
+      .map((el, idx) => {
+        if (el) return data[idx].id;
+      })
+      .filter((el) => el !== undefined),
   );
 }
 
@@ -62,79 +151,27 @@ function onMouseMove(
   });
 }
 
-function findPointed(
-  pos: { x: number; y: number },
-  data: Array<MapItem>,
-): Array<MapItem> {
-  return data.filter((item) => {
-    return (
-      pos.x > item.x &&
-      pos.x < item.width + item.x &&
-      pos.y > item.y &&
-      pos.y < item.height + item.y
-    );
-  });
-}
-
 function moveItem(
   id: string,
   moveVec: { x: number; y: number },
   data: Array<MapItem>,
   setter: (data: Array<MapItem>) => void,
 ) {
-  console.log(moveVec);
   setter(
     data.map((el) => {
       if (el.id === id) {
         return {
-          x: el.x + moveVec.x,
-          y: el.y + moveVec.y,
-          width: el.width,
-          height: el.height,
+          pos: new Rect(
+            el.pos.x + moveVec.x,
+            el.pos.y + moveVec.y,
+            el.pos.width,
+            el.pos.height,
+          ),
           id: el.id,
         };
       } else {
         return el;
       }
     }),
-  );
-}
-export default function Map(props: MapProps) {
-  const { data } = props;
-
-  const mapRoot = useRef<HTMLDivElement | null>(null);
-  const [items, setItems] = useState(data);
-  const moving: Array<string> = [];
-  const [movingElements, setMovingElements] = useState(moving);
-  return (
-    <div
-      className="relative w-full h-full"
-      onMouseDown={(e) => {
-        if (mapRoot.current) {
-          onMouseDown(e, mapRoot.current, items, setMovingElements);
-        }
-      }}
-      onMouseUp={(e) => {
-        onMouseUp(e, setMovingElements);
-      }}
-      onMouseMove={(e) => {
-        onMouseMove(e, movingElements, items, setItems);
-      }}
-    >
-      {items.map((item) => {
-        return (
-          <div key={item.id} ref={mapRoot}>
-            <MapItem
-              pos={{
-                x: item.x,
-                y: item.y,
-                height: item.height,
-                width: item.width,
-              }}
-            ></MapItem>
-          </div>
-        );
-      })}
-    </div>
   );
 }
