f1a3b013b314e86a3393ab23fb88b718d0bd7b12
KustelR <dmitriysstudios2@gmail.com>
2025-02-04 01:30:14 +0400

    changed map props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,22 @@
 
 import Image from "next/image";
 import Map from "@/components/Map";
+import { Rect, Vec2 } from "@/geometry";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
       <Map
-        data={[{ x: 10, y: 10, width: 30, height: 30, id: "dsfasdfds" }]}
+        data={[
+          {
+            pos: new Rect(10, 10, 30, 30),
+            id: "dsfasdfds",
+          },
+          {
+            pos: new Rect(50, 50, 30, 30),
+            id: "sfhgfsd",
+          },
+        ]}
       ></Map>
     </div>
   );
