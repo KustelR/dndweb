f08a11ede40fe6fa724850c67cc50e4272754a67
KustelR <dmitriysstudios2@gmail.com>
2025-02-04 07:19:06 +0400

    creating maps in MapControl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,22 @@
 import Image from "next/image";
 import Map from "@/components/Map";
 import { Rect, Vec2 } from "@/geometry";
+import Nav from "@/components/Nav";
+import MapControl from "@/components/MapControl";
+import { useState } from "react";
 
 export default function Home() {
+  const emptyMapData: MapData = { items: [], offset: new Vec2(0, 0) };
+  const [mapData, setMapData] = useState(emptyMapData);
+
   return (
-    <div className="w-full h-screen">
-      <Map
-        data={[
-          {
-            pos: new Rect(10, 10, 30, 30),
-            id: "dsfasdfds",
-          },
-          {
-            pos: new Rect(50, 50, 30, 30),
-            id: "sfhgfsd",
-          },
-        ]}
-      ></Map>
+    <div className="w-full h-screen flex flex-row">
+      <Nav>
+        <MapControl mapData={mapData} setMapData={setMapData} />
+      </Nav>
+      <div className="w-full h-full">
+        <Map setData={setMapData} data={mapData}></Map>
+      </div>
     </div>
   );
 }
