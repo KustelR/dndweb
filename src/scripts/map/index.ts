2b871f1ca134b8b553a94e359a73036e5acdcc64
KustelR <dmitriysstudios2@gmail.com>
2025-02-04 09:01:36 +0400

    removed not-existing export

--- src/scripts/map/index.ts
+++ src/scripts/map/index.ts
@@ -1,3 +1,2 @@
 export { default as findPointed } from "./findPointed";
-export { default as getRelativeClickPos } from "./getRelativeClickPos";
 export { default as renderItem } from "./renderItem";
