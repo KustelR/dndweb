68f9a523478e106d564d6eb0dedb98b2c3dc83e5
KustelR <dmitriysstudios2@gmail.com>
2025-02-05 00:33:56 +0400

    using LabeledInput

--- src/components/MapControl.tsx
+++ src/components/MapControl.tsx
@@ -1,6 +1,6 @@
 import { Rect, Vec2 } from "@/geometry";
 import React, { useEffect, useState } from "react";
-import LabeledInput from "./LabeledInput";
+import LabeledInput from "./ui/LabeledInput";
 
 export default function MapControl(props: {
   mapData: MapData;
@@ -24,25 +24,26 @@ export default function MapControl(props: {
     }
   }
   return (
-    <div>
-      <h3>Map Control</h3>
-      <ul>
+    <div className="bg-white/5 pb-2 w-60">
+      <h3 className="font-bold bg-white/5 border-b-2 px-1 border-neutral-700">
+        map control
+      </h3>
+      <ul className="px-1">
         <li>
           <form>
-            <label htmlFor="mapSrc">Map source</label>
-            <input
+            <LabeledInput
+              id="mapSrc"
+              label="map source"
               onChange={(event) => {
                 setMapSrc(event.target.value);
               }}
-              id="mapSrc"
-              type="text"
             />
           </form>
         </li>
         <li>
           <form
             action=""
-            className="[&>*]:block "
+            className="flex flex-col space-y-1"
             onSubmit={(e) => {
               e.preventDefault();
               pushItem();
@@ -51,31 +52,53 @@ export default function MapControl(props: {
             <LabeledInput
               label="name"
               onChange={(e) => {
-                setNewMapItem({ id: e.target.value, pos: itemPos });
+                setNewMapItem({
+                  id: e.target.value,
+                  pos: itemPos,
+                  fill: newMapItem ? newMapItem.fill : undefined,
+                });
               }}
               id="itemName"
             />
+            <div className="items-stretch space-x-1 grid grid-cols-2">
+              <LabeledInput
+                containerClass="overflow-hidden block col-span-1"
+                id="width"
+                onChange={(event) => {
+                  setItemPos(
+                    Object.assign(itemPos, {
+                      width: Number(event.target.value),
+                    }),
+                  );
+                }}
+              />
+              <LabeledInput
+                id="height"
+                containerClass="overflow-hidden block col-span-1"
+                onChange={(event) => {
+                  setItemPos(
+                    Object.assign(itemPos, {
+                      height: Number(event.target.value),
+                    }),
+                  );
+                }}
+              />
+            </div>
             <LabeledInput
-              id="width"
+              id="background"
               onChange={(event) => {
-                setItemPos(
-                  Object.assign(itemPos, {
-                    width: Number(event.target.value),
+                setNewMapItem(
+                  Object.assign({}, newMapItem, {
+                    fill: event.target.value,
                   }),
                 );
               }}
             />
-            <LabeledInput
-              id="height"
-              onChange={(event) => {
-                setItemPos(
-                  Object.assign(itemPos, {
-                    height: Number(event.target.value),
-                  }),
-                );
-              }}
+            <input
+              id="addItem"
+              className="bg-neutral-300 text-black"
+              type="submit"
             />
-            <input id="addItem" className="bg-white" type="submit" />
           </form>
         </li>
       </ul>
