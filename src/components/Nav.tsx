3bb9386c40fbf25c7b130ab338d3e3a26fa3df07
KustelR <dmitriysstudios2@gmail.com>
2025-02-04 07:19:26 +0400

    normal children prop

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { JSX, useState } from "react";
-import Link from "next/link";
+import React, { ReactNode, useState } from "react";
 
 type NavItem = {
   name: string;
@@ -9,29 +8,23 @@ type NavItem = {
 };
 
 type NavProps = {
-  children?: Array<string | JSX.Element | JSX.Element[]>;
+  children: ReactNode;
 };
 
 export default function Nav(props: NavProps) {
   const { children } = props;
   const [hidden, setHidden] = useState(false);
   return (
-    <nav className={`w-fit z-50 ${hidden ? "absolute h-full" : "relative"}`}>
+    <nav className={`w-fit h-full z-50 ${hidden ? "absolute" : "relative"}`}>
       <button
         className={`${hidden ? "" : "absolute"} right-0 w-10 h-full hover:bg-white/5`}
         onClick={() => {
           setHidden(!hidden);
         }}
       ></button>
-      <div className={`${hidden ? "w-0 hidden" : "w-40"}`}>
+      <div className={`${hidden ? "w-0 hidden" : "w-fit"}`}>
         <h2>Explorer</h2>
-        <ul>
-          {children
-            ? children.map((child) => {
-                return <li>{child}</li>;
-              })
-            : ""}
-        </ul>
+        {children}
       </div>
     </nav>
   );
