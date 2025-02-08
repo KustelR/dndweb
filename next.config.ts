cef37e262faf8568e32805ee99bc4e761bd3b015
KustelR <dmitriysstudios2@gmail.com>
2025-02-08 19:36:23 +0400

    cors for next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    const imagesApiHost = process.env.IMAGES_API_HOST;
+    if (!imagesApiHost) throw new Error("IMAGES_API_HOST must be set");
+    return [
+      {
+        source: "/api/images/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: imagesApiHost,
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
